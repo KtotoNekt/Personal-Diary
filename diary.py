0e6652c2edd110056be8b8b50e5e055870ce82f3
Ktoto <136707036+KtotoNekt@users.noreply.github.com>
2024-01-26 16:36:39 +0300

    Хранение записей в txt

--- diary.py
+++ diary.py
@@ -2,8 +2,7 @@ import os
 import zipfile
 
 from odf import text, teletype
-from odf.opendocument import load, OpenDocumentText
-from odf.style import Style, TextProperties, ParagraphProperties
+from odf.opendocument import load
 from os.path import abspath, join, exists, splitext
 from datetime import datetime
 
@@ -25,31 +24,14 @@ def _read_txt(filepath: str):
     with open(filepath, "r") as fp:
         content = ""
         for line in fp.readlines():
-            content += line + "\n"
+            content += line
 
     return content
 
 
-def _write_odt(filepath: str, content: str):
-    doc = OpenDocumentText()
-
-    for paragraph_content in content.split("\n"):
-        paragraph = text.P(text=paragraph_content)
-
-        new_style = Style(name="BaseStyle", family="paragraph")
-        text_properties = TextProperties(fontfamily="Times New Roman", fontsize="18pt")
-        paragraph_properties = ParagraphProperties(textindent="1cm")
-
-        new_style.addElement(text_properties)
-        new_style.addElement(paragraph_properties)
-
-        doc.styles.addElement(new_style)
-
-        paragraph.setAttribute("stylename", new_style)
-
-        doc.text.addElement(paragraph)
-
-    doc.save(filepath)
+def _write_txt(path: str, content: str):
+    with open(path, "w") as fp:
+        fp.write(content)
 
 
 def get_date(value: datetime):
@@ -57,7 +39,7 @@ def get_date(value: datetime):
 
 
 def _get_filepath(date: str):
-    return abspath(join("records", date + ".odt"))
+    return abspath(join("records", date + ".txt"))
 
 
 def exists_record(date: str) -> bool:
@@ -74,21 +56,21 @@ def import_record(date: str, filepath: str):
         case _:
             content = ""
 
-    _write_odt(_get_filepath(date), content)
+    _write_txt(_get_filepath(date), content)
 
 
 def read_record(date: str):
-    return _read_odt(_get_filepath(date))
+    return _read_txt(_get_filepath(date))
 
 
 def create_record(content: str):
     filename = _get_filepath(get_date(datetime.now()))
-    _write_odt(filename, content)
+    _write_txt(filename, content)
 
 
 def edit_record(date: str, content: str):
     filename = _get_filepath(date)
-    _write_odt(filename, content)
+    _write_txt(filename, content)
 
 
 def delete_record(date: str):
